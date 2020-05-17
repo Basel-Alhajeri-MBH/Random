f147145020803f641ef56639650fa59e5f7636fe
Basel Al_hajeri.MBH?.() <56924872+Basel-Alhajeri-MBH@users.noreply.github.com>
2020-05-17 07:26:38 +0300

    Random.js

--- JavaScript.js
+++ JavaScript.js
@@ -2,13 +2,14 @@
  * Created by Basel Mohammed Al_hajeri.MBH?.();
  * Copyright (C) 2020 MBH .All right reserved.
  */
-;this.self = this.globalThis || this.self || this.window || this;
+this.self = this.globalThis || this.self || this.window || this;
 (function(global, factory) {
     typeof exports==='object' && typeof module!=='undefined' ? factory(exports) : typeof define==='function' && define.amd ? define(['exports', factory]) : factory(global.Random = {});
 }(self, function(Export) {
     'use strict';
     var res = new Def(Date.now() / 1e+13);
     var all = ['rand', 'randInt'];
+    var map = [].map;
     res.met();
     Export.rand = function() {
         var finalResult = res.pro;
@@ -25,7 +26,7 @@
     else Export.toString = function() {
         return '[object Random]'; // just with toString in old commonJS;
     }.bind(self);
-    all.map(function(pro) {
+    map.call(all, function(pro) {
         Export[pro].toString = function() {
             return'function '+pro+'() { [MBH code] }';
         }.bind(self);
@@ -38,7 +39,7 @@
             for (θ = time.length - 1; θ >= 0; θ--) {
                 hz += time[ θ ];
             }
-            return Number('0.' + hz); //+time.split('').reverse().join('')); hmm thought no prob lol .. don't want to use any short ways
+            return Number('0.' + hz); //+time.split('').reverse().join('')); hmm thought no prob lol .. 
         };
         var _this = this;
         _this.pro = (delta() + (x || 1 / 7) * 3.141592653589793) % 1;
